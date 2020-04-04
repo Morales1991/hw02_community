b12a0d983047019a651e1dbbac47b97a46bc2a67
morales1991 <v.kulikov91@yandex.ru>
2020-04-04 10:39:52 +0300

    homework Publics v1.0

--- posts/models.py
+++ posts/models.py
@@ -9,11 +9,9 @@ class Group(models.Model):
     slug = models.SlugField(unique=True)
     description = models.TextField()
 
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post_author")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     group = models.ForeignKey(Group, on_delete=models.CASCADE, blank=True, null=True)
-
-
-
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render,get_object_or_404
+
 from .models import Post, Group
 
 
 def index(request):
     latest = Post.objects.order_by("-pub_date")[:11]
     return render(request, "index.html", {"posts":latest})
+
 
 def group_posts(request,slug):
     group = get_object_or_404 (Group, slug=slug)
