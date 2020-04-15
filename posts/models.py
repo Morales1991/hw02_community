0f990decf3edef57b3db666fcdd2019e55e92ebb
morales1991 <v.kulikov91@yandex.ru>
2020-04-15 17:59:48 +0300

    демо 4 спринта

--- posts/models.py
+++ posts/models.py
@@ -18,7 +18,7 @@ class Post(models.Model):
     def __str__(self):
         return self.text
       
-    text = models.TextField()
+    text = models.TextField(blank=True, null=True)
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     group = models.ForeignKey(Group, on_delete=models.CASCADE, blank=True, null=True)
--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("group/<slug>", views.group_posts, name="posts_group")
+    path("group/<slug>", views.group_posts, name="posts_group"),
+    path("new", views.new_post, name="new_post"),
 ]
